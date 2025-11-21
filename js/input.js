bdef112510dbf1c209b82fc0f026b6eaaa2634e6
Mike Whyle <111729086+threapchills@users.noreply.github.com>
2025-11-21 22:30:16 +0200

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,71 +1,58 @@
 /* THE NERVOUS SYSTEM (Input Handler)
-   This module listens to the whispers of the keyboard and the
-   movements of the mouse. It translates your physical will 
-   into digital spirit energy.
+   Now with Spacebar support for Jumping/Flying!
 */
 
 export class InputHandler {
     constructor() {
-        // The State of the Keys (True = Pressed, False = released)
         this.keys = {
             w: false,
             a: false,
             s: false,
             d: false,
-            space: false, // Just in case we need a jump or brake later
-            shift: false  // Maybe for a speed boost?
+            space: false, // JUMP / FLY
+            shift: false
         };
 
-        // The State of the Mouse
         this.mouse = {
             x: 0,
             y: 0,
             leftDown: false,
             rightDown: false,
-            isDragging: false // For moving islands later
+            isDragging: false 
         };
 
-        // Bind the event listeners to the window/document
         this._initListeners();
     }
 
     _initListeners() {
-        // 1. KEYBOARD EARS
         window.addEventListener('keydown', (e) => {
             const key = e.key.toLowerCase();
-            if (this.keys.hasOwnProperty(key)) {
-                this.keys[key] = true;
-            }
+            if (this.keys.hasOwnProperty(key)) this.keys[key] = true;
+            if (e.code === 'Space') this.keys.space = true; // Use code for Space
             if (e.key === 'Shift') this.keys.shift = true;
-            if (e.key === ' ') this.keys.space = true;
         });
 
         window.addEventListener('keyup', (e) => {
             const key = e.key.toLowerCase();
-            if (this.keys.hasOwnProperty(key)) {
-                this.keys[key] = false;
-            }
+            if (this.keys.hasOwnProperty(key)) this.keys[key] = false;
+            if (e.code === 'Space') this.keys.space = false;
             if (e.key === 'Shift') this.keys.shift = false;
-            if (e.key === ' ') this.keys.space = false;
         });
 
-        // 2. MOUSE EYES
-        // We need the canvas element to calculate relative coordinates
         const canvas = document.getElementById('gameCanvas');
 
         window.addEventListener('mousemove', (e) => {
-            // Get the canvas position on the screen to offset the mouse
             const rect = canvas.getBoundingClientRect();
-            
-            // Calculate mouse position relative to the canvas 
-            // (0,0 is top left of the game window)
-            this.mouse.x = e.clientX - rect.left;
-            this.mouse.y = e.clientY - rect.top;
+            // Scale mouse coordinates in case canvas is resized by CSS
+            const scaleX = canvas.width / rect.width;
+            const scaleY = canvas.height / rect.height;
+            this.mouse.x = (e.clientX - rect.left) * scaleX;
+            this.mouse.y = (e.clientY - rect.top) * scaleY;
         });
 
         window.addEventListener('mousedown', (e) => {
-            if (e.button === 0) this.mouse.leftDown = true;   // Left Click (Fireball)
-            if (e.button === 2) this.mouse.rightDown = true;  // Right Click (Drag Island)
+            if (e.button === 0) this.mouse.leftDown = true;   
+            if (e.button === 2) this.mouse.rightDown = true;  
         });
 
         window.addEventListener('mouseup', (e) => {
@@ -73,33 +60,9 @@ export class InputHandler {
             if (e.button === 2) this.mouse.rightDown = false;
         });
 
-        // PREVENT CONTEXT MENU
-        // We don't want a browser menu popping up when you try to drag an island!
         window.addEventListener('contextmenu', (e) => {
             e.preventDefault();
             return false;
         });
-    }
-
-    // A helper to get the "Vector" of movement based on keys
-    // Returns {x: -1/0/1, y: -1/0/1}
-    getMoveVector() {
-        let x = 0;
-        let y = 0;
-
-        if (this.keys.a) x -= 1;
-        if (this.keys.d) x += 1;
-        if (this.keys.w) y -= 1;
-        if (this.keys.s) y += 1;
-
-        // Normalize diagonal movement so you don't fly faster diagonally
-        // (Pythagoras would be very upset if we didn't do this)
-        if (x !== 0 && y !== 0) {
-            const factor = 1 / Math.sqrt(2);
-            x *= factor;
-            y *= factor;
-        }
-
-        return { x, y };
     }
 }
